1cca7363ff9854529435c54cccfb1b3fbf28384f
Chetan Hatnapure <chchetanrules7@gmail.com>
2023-08-15 23:11:22 +0530

    Initiliaze the MERN Project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+	console.log(`Running on ${port}`);
+});
